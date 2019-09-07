4c66d2fc6002aa292a6bce634c0078f4e769276d
Joel Fernandez <joelfernandez0306@gmail.com>
2019-09-06 17:25:28 -0700

    Added google calendar

--- src/pages/Layout/Calendar.js
+++ src/pages/Layout/Calendar.js
@@ -9,7 +9,12 @@ class Calendar extends Component {
                 <br />
                 <div className="ui fluid segment">
                     <h3 className="ui header">Calendar</h3>
-                    <p>CALENDAR</p>
+                    <div className="weeklycal">
+                    
+                    </div>
+                    <div className="monthlycal">
+                    <iframe className="cal" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=bmQwbjloZGQ4Nm5sNDQ3aWVydmg2NWs4NDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4uY2hyaXN0aWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uanVkYWlzbSNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F6BF26&color=%237CB342&color=%23009688&color=%230B8043"></iframe>
+                    </div>
                 </div>
                 <br />
                 <br />
